d936b510b66c3292d3db0252a1881a9274224d04
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-08-31 16:03:00 +0200

    Create getScores function and implement it on the refresh button

---
+++ src/get-scores.js
@@ -0,0 +1,7 @@
+const getScores = () => {
+  fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/gUVHf8jHXiHOwKDV1m7W/scores/')
+  .then( response => response.json())
+  .then( data => data.result);
+}
+
+export { getScores }
--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import './style.css';
 import { postScore } from './post-score';
+import { getScores } from './get-scores';
 
 const sendScore = document.getElementById('sendScore');
+const refresh = document.getElementById('refresh');
+let scores;
 
 sendScore.addEventListener('click', () => {
   const name = document.getElementById('name');
   const score = document.getElementById('score');
   postScore(name.value, score.value);
+});
+
+refresh.addEventListener('click', () => {
+  scores = getScores();
 })
