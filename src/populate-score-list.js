901f76a32e7e642db5ff351a725961153830685c
ShinobiWarior <63932912+ShinobiWarior@users.noreply.github.com>
2021-09-01 14:05:54 +0200

    Update modules

--- src/populate-score-list.js
+++ src/populate-score-list.js
@@ -1,9 +1,11 @@
 const populateScoreList = (scores) => {
   const list = document.getElementById('list');
   list.innerHTML = '';
+  scores = scores.sort((a, b) => b.score - a.score);
   scores.forEach((score) => {
     const field = document.createElement('li');
-    field.innerHTML = `<span>${score.user}</span> <span>${score.score}</span>`;
+    const formatedNumber = new Intl.NumberFormat('de-DE').format(score.score);
+    field.innerHTML = `<span>${score.user}</span> <span>${formatedNumber}</span>`;
     list.appendChild(field);
   });
 };
